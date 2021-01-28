c33b0e2bca07ba7f2ca2d220349755e8a074ec71
TebelloX <tebellonamo@gmail.com>
2021-01-29 00:54:54 +0200

    initial project setup

--- ContosoCrafts/ContosoCraftsWebsite/Models/Product.cs
+++ ContosoCrafts/ContosoCraftsWebsite/Models/Product.cs
@@ -7,5 +7,10 @@ namespace ContosoCraftsWebsite.Models
 {
     public class Product
     {
+        public string Id { get; set;  }
+        public string Maker { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
     }
 }

f51cc65be2f38eb6a41346ffb97eb5dc8c4d3fd1
TebelloX <tebellonamo@gmail.com>
2021-01-29 00:49:56 +0200

    initial project setup

---
+++ ContosoCrafts/ContosoCraftsWebsite/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCraftsWebsite.Models
+{
+    public class Product
+    {
+    }
+}

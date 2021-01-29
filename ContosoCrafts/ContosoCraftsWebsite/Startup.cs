5706e949c5fc5502dfa978b1be5c8f6c71b28f91
TebelloX <tebellonamo@gmail.com>
2021-01-29 22:04:47 +0200

    initial project setup

--- ContosoCrafts/ContosoCraftsWebsite/Startup.cs
+++ ContosoCrafts/ContosoCraftsWebsite/Startup.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContosoCraftsWebsite
+namespace ContosoCraftsWebSite
 {
     public class Startup
     {

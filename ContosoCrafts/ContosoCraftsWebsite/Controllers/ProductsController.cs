7a44114a5339eb7ffd4d964bb1fc60a83b4d2acb
TebelloX <tebellonamo@gmail.com>
2021-02-09 23:51:06 +0200

    connecting API endpoints

--- ContosoCrafts/ContosoCraftsWebsite/Controllers/ProductsController.cs
+++ ContosoCrafts/ContosoCraftsWebsite/Controllers/ProductsController.cs
@@ -4,12 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCraftsWebsite.Services;
 
 namespace ContosoCraftsWebsite.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        public ProductsController(JsonFileProductService productService)
+        {
+
+        }
+
+        public JsonFileProductService ProductService { get;  }
     }
 }
